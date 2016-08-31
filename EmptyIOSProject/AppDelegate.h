300b79727219eb5ab03424c2d964f6584ff4e118
wrl <515045622@qq.com>
2016-08-31 11:58:24 +0800

    first commit

---
+++ EmptyIOSProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EmptyIOSProject
+//
+//  Created by 王儒林 on 16/8/31.
+//  Copyright © 2016年 xingshulin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EmptyIOSProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EmptyIOSProject
+//
+//  Created by 王儒林 on 16/8/31.
+//  Copyright © 2016年 xingshulin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
